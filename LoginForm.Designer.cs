ce61360c8cab96a93b071ba9047a7be444c17388
Rianul Amin <ppsppspsspss@gmail.com>
2022-12-21 17:10:44 +0600

    hi

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -31,9 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.lblPasswordEmpty = new System.Windows.Forms.Label();
+            this.lblUsernameEmpty = new System.Windows.Forms.Label();
+            this.lblInvalidLogin = new System.Windows.Forms.Label();
+            this.btnLogin = new ComponentFactory.Krypton.Toolkit.KryptonButton();
             this.txtPassword = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
             this.txtUserName = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
-            this.btnLogin = new ComponentFactory.Krypton.Toolkit.KryptonButton();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -53,6 +56,9 @@
             this.panel2.BackColor = System.Drawing.Color.Transparent;
             this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.lblPasswordEmpty);
+            this.panel2.Controls.Add(this.lblUsernameEmpty);
+            this.panel2.Controls.Add(this.lblInvalidLogin);
             this.panel2.Controls.Add(this.btnLogin);
             this.panel2.Controls.Add(this.txtPassword);
             this.panel2.Controls.Add(this.txtUserName);
@@ -61,43 +67,41 @@
             this.panel2.Size = new System.Drawing.Size(434, 312);
             this.panel2.TabIndex = 0;
             // 
-            // txtPassword
+            // lblPasswordEmpty
             // 
-            this.txtPassword.Location = new System.Drawing.Point(82, 115);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(264, 34);
-            this.txtPassword.StateCommon.Border.Color1 = System.Drawing.Color.Black;
-            this.txtPassword.StateCommon.Border.Color2 = System.Drawing.Color.Black;
-            this.txtPassword.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.txtPassword.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            this.txtPassword.StateCommon.Border.Rounding = 20;
-            this.txtPassword.StateCommon.Border.Width = 1;
-            this.txtPassword.StateCommon.Content.Color1 = System.Drawing.Color.Gray;
-            this.txtPassword.StateCommon.Content.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.txtPassword.TabIndex = 1;
-            this.txtPassword.Text = "Password";
+            this.lblPasswordEmpty.AutoSize = true;
+            this.lblPasswordEmpty.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPasswordEmpty.ForeColor = System.Drawing.Color.Red;
+            this.lblPasswordEmpty.Location = new System.Drawing.Point(130, 96);
+            this.lblPasswordEmpty.Name = "lblPasswordEmpty";
+            this.lblPasswordEmpty.Size = new System.Drawing.Size(160, 16);
+            this.lblPasswordEmpty.TabIndex = 5;
+            this.lblPasswordEmpty.Text = "Password Can Not Be Empty";
+            this.lblPasswordEmpty.Visible = false;
             // 
-            // txtUserName
+            // lblUsernameEmpty
             // 
-            this.txtUserName.Location = new System.Drawing.Point(82, 51);
-            this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(264, 34);
-            this.txtUserName.StateCommon.Border.Color1 = System.Drawing.Color.Black;
-            this.txtUserName.StateCommon.Border.Color2 = System.Drawing.Color.Black;
-            this.txtUserName.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
-            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.txtUserName.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            this.txtUserName.StateCommon.Border.Rounding = 20;
-            this.txtUserName.StateCommon.Border.Width = 1;
-            this.txtUserName.StateCommon.Content.Color1 = System.Drawing.Color.Gray;
-            this.txtUserName.StateCommon.Content.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUserName.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.txtUserName.TabIndex = 0;
-            this.txtUserName.Text = "Username";
+            this.lblUsernameEmpty.AutoSize = true;
+            this.lblUsernameEmpty.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsernameEmpty.ForeColor = System.Drawing.Color.Red;
+            this.lblUsernameEmpty.Location = new System.Drawing.Point(130, 32);
+            this.lblUsernameEmpty.Name = "lblUsernameEmpty";
+            this.lblUsernameEmpty.Size = new System.Drawing.Size(164, 16);
+            this.lblUsernameEmpty.TabIndex = 4;
+            this.lblUsernameEmpty.Text = "Username Can Not Be Empty";
+            this.lblUsernameEmpty.Visible = false;
+            // 
+            // lblInvalidLogin
+            // 
+            this.lblInvalidLogin.AutoSize = true;
+            this.lblInvalidLogin.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInvalidLogin.ForeColor = System.Drawing.Color.Red;
+            this.lblInvalidLogin.Location = new System.Drawing.Point(141, 174);
+            this.lblInvalidLogin.Name = "lblInvalidLogin";
+            this.lblInvalidLogin.Size = new System.Drawing.Size(139, 16);
+            this.lblInvalidLogin.TabIndex = 3;
+            this.lblInvalidLogin.Text = "Invalid Login Credentials";
+            this.lblInvalidLogin.Visible = false;
             // 
             // btnLogin
             // 
@@ -160,6 +164,49 @@
             this.btnLogin.StateTracking.Border.Width = 1;
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Values.Text = "Login";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(82, 115);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(264, 34);
+            this.txtPassword.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.txtPassword.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.txtPassword.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.txtPassword.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
+            this.txtPassword.StateCommon.Border.Rounding = 20;
+            this.txtPassword.StateCommon.Border.Width = 1;
+            this.txtPassword.StateCommon.Content.Color1 = System.Drawing.SystemColors.GrayText;
+            this.txtPassword.StateCommon.Content.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter_1);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave_1);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(82, 51);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(264, 34);
+            this.txtUserName.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.txtUserName.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.txtUserName.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.txtUserName.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
+            this.txtUserName.StateCommon.Border.Rounding = 20;
+            this.txtUserName.StateCommon.Border.Width = 1;
+            this.txtUserName.StateCommon.Content.Color1 = System.Drawing.Color.Gray;
+            this.txtUserName.StateCommon.Content.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.txtUserName.TabIndex = 0;
+            this.txtUserName.Text = "Username";
+            this.txtUserName.Enter += new System.EventHandler(this.txtUserName_Enter);
+            this.txtUserName.Leave += new System.EventHandler(this.txtUserName_Leave_1);
             // 
             // LoginForm
             // 
@@ -184,6 +231,9 @@
         private ComponentFactory.Krypton.Toolkit.KryptonTextBox txtUserName;
         private ComponentFactory.Krypton.Toolkit.KryptonTextBox txtPassword;
         private ComponentFactory.Krypton.Toolkit.KryptonButton btnLogin;
+        private System.Windows.Forms.Label lblInvalidLogin;
+        private System.Windows.Forms.Label lblPasswordEmpty;
+        private System.Windows.Forms.Label lblUsernameEmpty;
     }
 }
 
--- LoginForm.cs
+++ LoginForm.cs
@@ -12,9 +12,77 @@ namespace CareForPaws
 {
     public partial class LoginForm : Form
     {
+
+        private DataAccess Da { get; set; }
+
         public LoginForm()
         {
             InitializeComponent();
+            this.Da = new DataAccess();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+                var sql = "select * from UserInfo where Username = '" + this.txtUserName.Text + "' and Password = '" + this.txtPassword.Text + "';";
+                var ds = this.Da.ExecuteQuery(sql);
+
+                if (ds.Tables[0].Rows.Count == 1)
+                {
+                    if (ds.Tables[0].Rows[0][7].ToString() == "Admin")
+                        new AdminHome().Show();
+                    else if (ds.Tables[0].Rows[0][7].ToString() == "Seller")
+                        new SellerHome().Show();
+                }
+                else
+                {
+                    lblInvalidLogin.Visible = true;
+                }
+        }
+
+        private void txtUserName_Enter(object sender, EventArgs e)
+        {
+
+            if (this.txtUserName.Text == "Username") {
+
+                this.txtUserName.Text = "";
+                this.txtUserName.StateCommon.Content.Color1 = System.Drawing.SystemColors.ActiveCaptionText;
+
+            }
+
+        }
+
+        private void txtUserName_Leave_1(object sender, EventArgs e)
+        {
+            if (this.txtUserName.Text == "")
+            {
+
+                this.txtUserName.Text = "Username";
+                this.txtUserName.StateCommon.Content.Color1 = System.Drawing.SystemColors.GrayText;
+
+            }
+        }
+
+        private void txtPassword_Enter_1(object sender, EventArgs e)
+        {
+            if (this.txtPassword.Text == "Password")
+            {
+
+                this.txtPassword.Text = "";
+                this.txtPassword.StateCommon.Content.Color1 = System.Drawing.SystemColors.ActiveCaptionText;
+
+            }
+        }
+
+        private void txtPassword_Leave_1(object sender, EventArgs e)
+        {
+            if (this.txtPassword.Text == "")
+            {
+
+                this.txtPassword.Text = "Password";
+                this.txtPassword.StateCommon.Content.Color1 = System.Drawing.SystemColors.GrayText;
+
+            }
+        }
+
     }
 }

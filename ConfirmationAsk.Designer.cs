cff734debe1af37edf9595e5171c3c37aff358d0
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2022-12-25 21:46:11 +0600

    center

--- ConfirmationAsk.Designer.cs
+++ ConfirmationAsk.Designer.cs
@@ -137,6 +137,7 @@
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ConfirmationAsk";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Attention Required";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ConfirmationAsk_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();

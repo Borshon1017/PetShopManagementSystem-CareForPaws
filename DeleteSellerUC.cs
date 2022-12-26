f2fdbf77d547befb607a8d0a5ae2d6ed7d761d85
Rianul Amin <ppsppspsspss@gmail.com>
2022-12-26 20:49:31 +0600

    h

--- DeleteSellerUC.cs
+++ DeleteSellerUC.cs
@@ -136,21 +136,9 @@ namespace CareForPaws
         {
             if (dgvSeller.Columns[e.ColumnIndex].Name.Equals("DeleteButton"))
             {
-                
-                ConfirmationAsk ask = new ConfirmationAsk();
-                
-                if (askStatic == true)
-                {
-                    var sql = "update UserInfo set Status = 'Inactive' where U_ID  = '" + this.dgvSeller.CurrentRow.Cells["U_ID"].Value.ToString() + "' and Role = 'Seller';";
-                    this.Da.ExecuteDMLQuery(sql);
-                    this.PopulateGridView();
-                }
-                else if (askStatic == false)
-                {
-                    return;
-                }
-
-
+                var sql = "update UserInfo set Status = 'Inactive' where U_ID  = '" + this.dgvSeller.CurrentRow.Cells["U_ID"].Value.ToString() + "' and Role = 'Seller';";
+                this.Da.ExecuteDMLQuery(sql);
+                this.PopulateGridView();
             }
         }
 
